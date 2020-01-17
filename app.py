d924b6b32979ef0c737b1d2da0862a29fcf27625
squtfever <kingdaano@gmail.com>
2020-01-17 15:05:13 +0100

    Code cleanup + commenting

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ import PySimpleGUI as sg
 import time
 from services import GetValue, selectFloor
 
-CurrentFloor = 0
-chosenFloor = []
-callFloor = []
-passengers= 0
+# Initialize variables for global scope
+CurrentFloor = 0        # Current floor of the elevator
+chosenFloor = []        # List of all the floors that people have chosen
+callFloor = []          # List of floors where people have called an elevator
+passengers = 0         # Amount of passengers currently in the elevator
+
 sg.theme("DarkAmber")
 
+# Defining the layout for the main display
 mainLayout = [
     [sg.Text('Current \n position:'), sg.Text('Call elevator:', justification="right")],
     [sg.T('', key='5', size=(5, 1)), sg.Button('5', size=(11, 1))],
@@ -22,15 +25,14 @@ mainLayout = [
 window = sg.Window('Elevator', mainLayout)
 
 while True:
+    # Check if the elevator is idle
     if len(chosenFloor) == 0 and len(callFloor) == 0:
         event, values = window.read()
         if len(event) > 0:
             callFloor.append(GetValue(event))
     while len(chosenFloor) > 0 or len(callFloor) > 0:
-        print("48")
         # Go to chosen floors first
         if len(chosenFloor) > 0:
-            print("51")
             if chosenFloor[0] < CurrentFloor:
                 window[str(CurrentFloor)].update("")
                 CurrentFloor -= 1;
@@ -39,9 +41,8 @@ while True:
                 window[str(CurrentFloor)].update("")
                 CurrentFloor += 1;
                 window[str(CurrentFloor)].update("O")
-        # Go to called floors 2nd
+        # Go to called floors after all chosen floors are gone
         else:
-            print("62")
             if callFloor[0] < CurrentFloor:
                 window[str(CurrentFloor)].update("")
                 CurrentFloor -= 1;
@@ -50,11 +51,10 @@ while True:
                 window[str(CurrentFloor)].update("")
                 CurrentFloor += 1;
                 window[str(CurrentFloor)].update("O")
-        print("71")
+        # Check if the elevator is on a desired floor
         if (CurrentFloor in callFloor) or (CurrentFloor in chosenFloor):
-            print("75")
+            # check if a new passenger enters the elevator
             if CurrentFloor in callFloor:
-                print("81")
                 try:
                     chosenFloor.append(GetValue(selectFloor()))
                     passengers += 1
@@ -69,8 +69,9 @@ while True:
                 passengers -= 1
             except:
                 pass
-
+        # Update passenger count
         window['PassengerCounter'].update(passengers)
+        # Wait for input for 1 second then continue
         event, values = window.Read(timeout=1000, timeout_key="__TIMEOUT__")
         if event != "__TIMEOUT__":
             time.sleep(0.5)
--- services.py
+++ services.py
@@ -1,13 +1,15 @@
 import PySimpleGUI as sg
 
-def GetValue(event):
-    if (event[0] == "-"):
+
+def GetValue(event):    # this function formats the input to ints
+    if event[0] == "-":
         return int("-" + event[1])
     else:
        return int(event[0])
 
-def selectFloor():
-    print(32)
+
+def selectFloor():  # function to get a floor from a new passenger
+    # define window layout
     chooseFloor = [
         [sg.Text('Choose the floor:')],
         [sg.Button('5', size=(11, 1))],
@@ -19,9 +21,9 @@ def selectFloor():
         [sg.Button('-1', size=(11, 1))],
         [sg.Button('-2', size=(11, 1))]
     ]
+    # open window and check for input
     choosewindow = sg.Window('Choose Floor', chooseFloor)
-    print(39)
     event, value = choosewindow.read()
-    print(42)
     choosewindow.close()
+    #return input
     return event
